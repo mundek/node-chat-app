393abc74ecf3b0d76d8b81ce98c9da69e974d70b
Ray Meredith <ray.meredith@gmail.com>
2017-03-20 17:25:41 -0500

    Fixed minor error

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public/');
 // console.log(`publicPath: ${publicPath}`);
-const port = process.env.PATH || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
